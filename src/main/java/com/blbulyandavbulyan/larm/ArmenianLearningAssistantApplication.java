873b549e0afd64f65d5e4471da678c5e04a8a329
david <blbulyandavbulyan@gmail.com>
2026-05-30 22:00:49 +0400

    small fixes of warnings

--- src/main/java/com/blbulyandavbulyan/larm/ArmenianLearningAssistantApplication.java
+++ src/main/java/com/blbulyandavbulyan/larm/ArmenianLearningAssistantApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArmenianLearningAssistantApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(ArmenianLearningAssistantApplication.class, args);
 	}
 
--- src/main/java/com/blbulyandavbulyan/larm/phrase/service/IllegalLanguageCode.java
+++
@@ -1,7 +0,0 @@
-package com.blbulyandavbulyan.larm.phrase.service;
-
-public class IllegalLanguageCode extends IllegalArgumentException {
-    public IllegalLanguageCode(String message) {
-        super(message);
-    }
-}
