4e9324a0879c5a61265aad41ca552e61db950d77
David Blbulyan <blbulyandavbulyan@gmail.com>
2026-06-26 16:39:16 +0400

    attempt to fix lazy initialization exception in  PhrasesControllerGetPhrasesIT.getPhrases_firstPage

--- src/main/java/com/blbulyandavbulyan/larm/dao/repository/PhraseRepository.java
+++ src/main/java/com/blbulyandavbulyan/larm/dao/repository/PhraseRepository.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import com.blbulyandavbulyan.larm.dao.entities.Phrase;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,5 +16,6 @@ public interface PhraseRepository extends CrudRepository<Phrase, UUID> {
     @Query("SELECT p.phrase FROM Phrase p WHERE p.phrase IN :phrases")
     Set<String> findExistingPhrases(@Param("phrases") Collection<String> phrases);
 
+    @EntityGraph(attributePaths = {"mediaSet", "translations"})
     Page<Phrase> findAll(Pageable pageable);
 }
