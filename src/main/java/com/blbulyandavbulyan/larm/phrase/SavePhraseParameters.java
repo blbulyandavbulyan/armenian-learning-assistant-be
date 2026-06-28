b6f59a8d5298e535ad2e5ad1a8db70324ba8d525
David Blbulyan <blbulyandavbulyan@gmail.com>
2026-06-28 17:13:30 +0400

    fixed todo comments in SavePhraseParameters, StoreDialogueParameters, noticed same problem in DialogueRecord, PhraseRecord

--- src/main/java/com/blbulyandavbulyan/larm/phrase/SavePhraseParameters.java
+++ src/main/java/com/blbulyandavbulyan/larm/phrase/SavePhraseParameters.java
@@ -1,12 +1,14 @@
 package com.blbulyandavbulyan.larm.phrase;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import lombok.Builder;
+import org.jspecify.annotations.NonNull;
 
 @Builder(toBuilder = true)
-// TODO sonar warning java:S6218
 public record SavePhraseParameters(
         UUID id,
         String phrase,
@@ -16,4 +18,35 @@ public record SavePhraseParameters(
         List<CreateTranslationParameters> translations,
         List<CreateMediaResource> mediaResources) {
 
+    @SuppressWarnings({"DeconstructionCanBeUsed", "java:S6878"})
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof SavePhraseParameters that)) {
+            return false;
+        }
+        return Objects.equals(id, that.id) &&
+                Objects.equals(phrase, that.phrase) &&
+                Objects.equals(isoLanguageCode, that.isoLanguageCode) &&
+                Objects.equals(transcription, that.transcription) &&
+                Arrays.equals(embedding, that.embedding) &&
+                Objects.equals(translations, that.translations) &&
+                Objects.equals(mediaResources, that.mediaResources);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, phrase, isoLanguageCode, transcription, translations, mediaResources);
+        result = 31 * result + Arrays.hashCode(embedding);
+        return result;
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "SavePhraseParameters{id=%s, phrase='%s', isoLanguageCode='%s', transcription='%s', embedding=%s, translations=%s, mediaResources=%s}"
+                .formatted(id, phrase, isoLanguageCode, transcription, Arrays.toString(embedding), translations, mediaResources);
+    }
 }
--- src/test/java/com/blbulyandavbulyan/larm/phrase/dao/projections/DialogueRecord.java
+++ src/test/java/com/blbulyandavbulyan/larm/phrase/dao/projections/DialogueRecord.java
@@ -1,11 +1,14 @@
 package com.blbulyandavbulyan.larm.phrase.dao.projections;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import com.blbulyandavbulyan.larm.dao.entities.Dialogue;
+import org.jspecify.annotations.NonNull;
 
 public record DialogueRecord(
         UUID id,
@@ -30,5 +33,34 @@ public record DialogueRecord(
 
     private static Set<DialoguePhraseRecord> getPhraseRecords(Dialogue dialogue) {
         return dialogue.getDialoguePhrases().stream().map(DialoguePhraseRecord::new).collect(Collectors.toSet());
+    }
+
+    @SuppressWarnings({"DeconstructionCanBeUsed", "java:S6878"})
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof DialogueRecord that)) {
+            return false;
+        }
+        return Objects.equals(id, that.id)
+                && Objects.equals(createdAt, that.createdAt)
+                && Objects.deepEquals(embedding, that.embedding)
+                && Objects.equals(title, that.title)
+                && Objects.equals(speakers, that.speakers)
+                && Objects.equals(dialoguePhrases, that.dialoguePhrases);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, speakers, dialoguePhrases, createdAt, Arrays.hashCode(embedding));
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "DialogueRecord{id=%s, title=%s, speakers=%s, dialoguePhrases=%s, createdAt=%s, embedding=%s}"
+                .formatted(id, title, speakers, dialoguePhrases, createdAt, Arrays.toString(embedding));
     }
 }
