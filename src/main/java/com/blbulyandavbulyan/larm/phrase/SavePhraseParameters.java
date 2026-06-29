472194e660c8abf8013d03e11a1bb415d98e8e3c
David Blbulyan <blbulyandavbulyan@gmail.com>
2026-06-29 17:01:26 +0400

    dropped equals, hashCode and toString, they were added by me because of sonar warning, we added embedding in PhraseRecord and in SavePhraseParameters, and because this is an array, it has no proper equals, hashCode and toString methods, so this was dropped

--- src/main/java/com/blbulyandavbulyan/larm/phrase/SavePhraseParameters.java
+++ src/main/java/com/blbulyandavbulyan/larm/phrase/SavePhraseParameters.java
@@ -1,11 +1,9 @@
 package com.blbulyandavbulyan.larm.phrase;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 import lombok.Builder;
-import org.jspecify.annotations.NonNull;
 
 @Builder(toBuilder = true)
 public record SavePhraseParameters(
@@ -16,32 +14,4 @@ public record SavePhraseParameters(
         List<CreateTranslationParameters> translations,
         List<CreateMediaResource> mediaResources) {
 
-    @SuppressWarnings({"DeconstructionCanBeUsed", "java:S6878"})
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SavePhraseParameters that)) {
-            return false;
-        }
-        return Objects.equals(id, that.id)
-                && Objects.equals(phrase, that.phrase)
-                && Objects.equals(isoLanguageCode, that.isoLanguageCode)
-                && Objects.equals(transcription, that.transcription)
-                && Objects.equals(translations, that.translations)
-                && Objects.equals(mediaResources, that.mediaResources);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, phrase, isoLanguageCode, transcription, translations, mediaResources);
-    }
-
-    @Override
-    @NonNull
-    public String toString() {
-        return "SavePhraseParameters{id=%s, phrase='%s', isoLanguageCode='%s', transcription='%s', translations=%s, mediaResources=%s}"
-                .formatted(id, phrase, isoLanguageCode, transcription, translations, mediaResources);
-    }
 }
--- src/test/java/com/blbulyandavbulyan/larm/phrase/dao/projections/PhraseRecord.java
+++ src/test/java/com/blbulyandavbulyan/larm/phrase/dao/projections/PhraseRecord.java
@@ -1,13 +1,11 @@
 package com.blbulyandavbulyan.larm.phrase.dao.projections;
 
-import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 import com.blbulyandavbulyan.larm.dao.entities.Phrase;
 import com.blbulyandavbulyan.larm.dao.entities.PhraseStatus;
-import org.jspecify.annotations.NonNull;
 
 public record PhraseRecord(
         UUID id,
@@ -30,34 +28,4 @@ public record PhraseRecord(
         return phrase.getTranslations().stream().map(TranslationRecord::new).collect(Collectors.toSet());
     }
 
-    @SuppressWarnings({"DeconstructionCanBeUsed", "java:S6878"})
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof PhraseRecord that)) {
-            return false;
-        }
-        return Objects.equals(id, that.id)
-                && Objects.equals(phrase, that.phrase)
-                && status == that.status
-                && Objects.equals(transcription, that.transcription)
-                && Objects.equals(isoLanguageCode, that.isoLanguageCode)
-                && Objects.equals(mediaSet, that.mediaSet)
-                && Objects.equals(translations, that.translations);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, status, isoLanguageCode, phrase, transcription,
-                translations, mediaSet);
-    }
-
-    @Override
-    @NonNull
-    public String toString() {
-        return "PhraseRecord{id=%s, status=%s, isoLanguageCode='%s', phrase='%s', transcription='%s', translations=%s, mediaSet=%s}"
-                .formatted(id, status, isoLanguageCode, phrase, transcription, translations, mediaSet);
-    }
 }
